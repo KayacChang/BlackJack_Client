2b0536953916fa8842ff5d430eed0575dfb44ec3
kayac <egg734631@gmail.com>
2020-05-17 01:18:42 +0800

    Remove tab active

--- src/pages/UI/index.tsx
+++ src/pages/UI/index.tsx
@@ -1,24 +1,14 @@
 import React, { useState, ReactNode } from "react";
 import { Flex } from "../../layouts/Flex";
 import Menu from "./Menu";
-import { Play, Settings, Info, Clock, LogOut } from "react-feather";
+import { Settings, Info, Clock, LogOut } from "react-feather";
 import SettingsPage from "./Settings";
 import HistoryPage from "./History";
 import GameRulesPage from "./GameRules";
 
-type Option = "game" | "rules" | "settings" | "history" | "home";
-
 export default function UI() {
   //
-  const [key, setKey] = useState<Option>("game");
-
-  const page = {
-    game: undefined,
-    rules: <GameRulesPage />,
-    settings: <SettingsPage />,
-    history: <HistoryPage />,
-    home: undefined,
-  }[key];
+  const [page, setPage] = useState<ReactNode>();
 
   return (
     <Flex className="fixedPage">
@@ -26,31 +16,21 @@ export default function UI() {
         page={page}
         options={[
           {
-            active: key === "game",
-            icon: <Play color={key === "game" ? "#3071a9" : "white"} />,
-            title: "game",
-            onClick: () => setKey("game"),
-          },
-          {
-            active: key === "rules",
-            icon: <Info color={key === "rules" ? "#3071a9" : "white"} />,
+            icon: <Info color={"white"} />,
             title: "rules",
-            onClick: () => setKey("rules"),
+            onClick: () => setPage(<GameRulesPage />),
           },
           {
-            active: key === "settings",
-            icon: <Settings color={key === "settings" ? "#3071a9" : "white"} />,
+            icon: <Settings color={"white"} />,
             title: "settings",
-            onClick: () => setKey("settings"),
+            onClick: () => setPage(<SettingsPage />),
           },
           {
-            active: key === "history",
-            icon: <Clock color={key === "history" ? "#3071a9" : "white"} />,
+            icon: <Clock color={"white"} />,
             title: "history",
-            onClick: () => setKey("history"),
+            onClick: () => setPage(<HistoryPage />),
           },
           {
-            active: key === "home",
             icon: <LogOut color="white" />,
             title: "home",
             onClick: () => console.log("home"),
