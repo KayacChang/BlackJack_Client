98aee1a344dc360b3610d8e54b67f2d8d1181b4f
kayac <egg734631@gmail.com>
2020-06-14 20:33:45 +0800

    Add state type in ui

--- src/ui/UI.tsx
+++ src/ui/UI.tsx
@@ -4,16 +4,31 @@ import Status from './status';
 import Detail from './detail';
 import Bet from './bet';
 import Decision from './decision';
+import { useSelector } from 'react-redux';
+import { AppState } from '../store';
+import { GAME } from '../models';
+
+function Control() {
+  const { state } = useSelector((state: AppState) => state.game);
+
+  if (state.type === GAME.BET_START) {
+    return <Bet />;
+  }
+
+  if (state.type === GAME.TURN) {
+    return <Decision />;
+  }
+
+  return <></>;
+}
 
 export default function UI() {
-  //
   return (
     <div className="fixedPage" style={{ pointerEvents: 'none' }}>
       <Menu />
       <Status />
       <Detail />
-      {/* <Bet /> */}
-      {/* <Decision /> */}
+      {/* <Control /> */}
     </div>
   );
 }
