fc503fffc6efc584c963bebd5919cefbf4fa32eb
kayac <egg734631@gmail.com>
2020-06-16 00:27:11 +0800

    Add on seat click

--- src/game/main/seats.ts
+++ src/game/main/seats.ts
@@ -1,18 +1,24 @@
 import { Container, Sprite } from 'pixi.js';
 import RES from '../assets';
 import { SEAT } from '../../models';
-import { wait, throttleBy } from '../../utils';
+import { throttleBy } from '../../utils';
 import services from '../../services';
 
+interface Prop {
+  id: SEAT;
+  x: number;
+  y: number;
+}
+
 const seats = [
-  { name: SEAT.A, x: 15 / 100, y: 58 / 100 },
-  { name: SEAT.B, x: 30 / 100, y: 75 / 100 },
-  { name: SEAT.C, x: 50 / 100, y: 82 / 100 },
-  { name: SEAT.D, x: 70 / 100, y: 75 / 100 },
-  { name: SEAT.E, x: 85 / 100, y: 58 / 100 },
+  { id: SEAT.A, x: 15 / 100, y: 58 / 100 },
+  { id: SEAT.B, x: 30 / 100, y: 75 / 100 },
+  { id: SEAT.C, x: 50 / 100, y: 82 / 100 },
+  { id: SEAT.D, x: 70 / 100, y: 75 / 100 },
+  { id: SEAT.E, x: 85 / 100, y: 58 / 100 },
 ];
 
-function Seat() {
+function Seat({ id, x, y }: Prop) {
   const it = new Container();
 
   const normal = RES.get('SELECT_SEAT_NORMAL').texture;
@@ -26,7 +32,22 @@ function Seat() {
   it.buttonMode = true;
   it.interactive = true;
 
+  it.name = SEAT[id];
+  it.x = x;
+  it.y = y;
+
+  it.on('pointerdown', throttleBy(onSeatClick(id)));
+
   return it;
+}
+
+function onSeatClick(seat: SEAT) {
+  //
+  return async function () {
+    const seats = await services.joinSeat(seat);
+
+    console.log(seats);
+  };
 }
 
 export default function Seats() {
@@ -34,23 +55,14 @@ export default function Seats() {
   it.name = 'seats';
 
   it.once('added', ({ width, height }: Container) => {
-    for (const { name, x, y } of seats) {
-      const seat = Seat();
-
-      seat.name = SEAT[name];
-      seat.x = width * x;
-      seat.y = height * y;
+    for (const { id, x, y } of seats) {
+      const seat = Seat({
+        id: id,
+        x: width * x,
+        y: height * y,
+      });
 
       it.addChild(seat);
-
-      seat.on(
-        'pointerdown',
-        throttleBy(async () => {
-          const seats = await services.joinSeat(name);
-
-          console.log(seats);
-        })
-      );
     }
   });
 
