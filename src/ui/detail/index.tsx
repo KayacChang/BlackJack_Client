f8d3d450f5e01021efbd56b0ce85c872ef201608
kayac <egg734631@gmail.com>
2020-07-17 20:11:35 +0800

    Add history

--- src/ui/detail/index.tsx
+++ src/ui/detail/index.tsx
@@ -1,15 +1,14 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { Home } from 'react-feather';
 import { Button } from '../components/button/Button';
 import styles from './Detail.module.scss';
 import { useSelector } from 'react-redux';
-import { AppState } from '../../store';
+import store, { AppState } from '../../store';
 import { currency } from '../../utils';
 import { useSpring, animated } from 'react-spring';
 import { Expo } from 'gsap';
 import { useNavigate } from 'react-router-dom';
 import services from '../../services';
-import { times, identity } from 'ramda';
 
 function Back() {
   const navTo = useNavigate();
@@ -46,7 +45,7 @@ function Field({ title, value }: Props) {
   );
 }
 
-function History() {
+function History({ history = [] }: { history?: string[] }) {
   const [isOpen, toggle] = useState(false);
 
   const onToggle = useCallback(() => {
@@ -55,20 +54,18 @@ function History() {
 
   return (
     <div className={styles.history} onClick={onToggle}>
+      <div className={styles.content} style={{ width: (isOpen ? 100 : 0) + '%' }}>
+        {history.slice(0, 20).map((res, index) => (
+          <div key={String(index + 1).padStart(2, '0')} className={styles.record}>
+            <h5>{String(index + 1).padStart(2, '0')}</h5>
+            <h4>{res}</h4>
+          </div>
+        ))}
+      </div>
+
       <div className={styles.toggle}>
         <h5>History</h5>
       </div>
-
-      {isOpen && (
-        <div className={styles.content}>
-          {times(identity, 20).map((num) => (
-            <div className={styles.record}>
-              <h5>{String(num + 1).padStart(2, '0')}</h5>
-              <h4>25</h4>
-            </div>
-          ))}
-        </div>
-      )}
     </div>
   );
 }
@@ -77,6 +74,7 @@ export default function RoomDetail() {
   const roomID = useSelector((state: AppState) => state.game.room);
   const roundID = useSelector((state: AppState) => state.game.round);
   const { max, min } = useSelector((state: AppState) => state.game.bet);
+  const history = useSelector((state: AppState) => state.room.find(({ id }) => id === roomID)?.history);
 
   return (
     <div className={styles.detail}>
@@ -87,7 +85,7 @@ export default function RoomDetail() {
         <Field title={'bet'} value={`${currency(min)} - ${currency(max)}`} />
       </div>
 
-      <History />
+      <History history={history} />
     </div>
   );
 }
